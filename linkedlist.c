fda7e0966fa9e19b3ffa75934280ae723bae916f
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-05-14 23:33:53 +0530

    insert at

--- linkedlist.c
+++ linkedlist.c
@@ -11,3 +11,42 @@ Node_ptr create_node(Element element){
   new_node->next = NULL;
   return new_node;
 }
+
+Status insert_at(List_ptr list, Element element, int position){
+  Status operation_status;
+  Node_ptr node = create_node(element);
+    operation_status = node == NULL ? Failure : Success;
+    operation_status = position < 0 || position > list->length ? Failure : operation_status;
+    if (operation_status == Failure)
+    {
+      return operation_status;
+    }
+    if(list->length==0){
+      list->first = list->last = element;
+  list->length=1;
+      return operation_status;
+    }
+    if (position==0)
+    {
+      node->next= list->first;
+      list->first = node;
+      list->length++;
+       return operation_status;
+    }
+    if (position==list->length)
+    {
+      list->last->next = node;
+      list->last = node;
+      list->length++;
+       return operation_status;
+    }
+    Node_ptr curr_node = list->first;
+    for (int i = 0; i < position-1; i++)
+    {
+      curr_node = curr_node->next;
+    }
+    node->next=curr_node->next;
+    curr_node->next = node;
+    list->length++;
+    return operation_status;
+}
