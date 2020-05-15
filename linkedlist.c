d2655a84a0de2e3ac7e6cde3d304a8f76d141d3a
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-05-15 11:49:45 +0530

    bug fised in rduce

--- linkedlist.c
+++ linkedlist.c
@@ -102,13 +102,14 @@ List_ptr filter(List_ptr list, Predicate predicate_function){
 }
 
 Element reduce(List_ptr list, Element element, Reducer reducer_function){
+  Element result=element;
   Node_ptr curr_node = list->first;
   for (int i = 0; i < list->length; i++)
   {
-    element = reducer_function(element,curr_node->element);
+    result = reducer_function(result,curr_node->element);
     curr_node = curr_node->next;
   }
-  return element;
+  return result;
 }
 
 void forEach(List_ptr list, ElementProcessor processor){
